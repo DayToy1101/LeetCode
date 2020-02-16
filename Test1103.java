6000fc664ff8f074c302294627cab4c9b5e8cd78
a <2573263498@qq.com>
2020-02-16 20:22:40 +0800

    20200216

---
+++ Test1103.java
@@ -0,0 +1,10 @@
+public class Test1103{
+	public static void main(String args[]) {
+        int i = 7;
+        do {
+            System.out.println(--i);
+            --i;
+        } while (i != 0);
+        System.out.println(i);
+    }
+}
