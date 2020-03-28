3a1bbb18d138ee50b5899870295f888bae62fb5d
a <2573263498@qq.com>
2020-03-28 13:25:24 +0800

    20200328

---
+++ Find.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public boolean Find(int target, int [][] array) {
+        int rows = array.length;
+        if(rows == 0){
+            return false;
+        }
+        int cols = array[0].length;
+        if(cols == 0){
+            return false;
+        }
+        //从左下角开始查找
+        int row = rows-1;
+        int col = 0;
+        while(row>=0&&col<cols){
+            if(array[row][col]<target){
+                col++;
+            }else if(array[row][col]>target){
+                row--;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
